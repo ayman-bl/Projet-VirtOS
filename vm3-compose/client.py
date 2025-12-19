6ed538a9bf53083450fa6dc74229c59ddb2f5b71
ayman <aymanbelasri4@gmail.com>
2025-12-19 23:53:21 +0100

    client encode fix

--- vm3-compose/client.py
+++ vm3-compose/client.py
@@ -4,4 +4,7 @@ client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 client.connect(("192.168.200.28",9999)) 
 
-client.encode("utf-8").sendall()
+msg='hi'
+data=msg.encode("utf-8")
+
+client.sendall(data)
