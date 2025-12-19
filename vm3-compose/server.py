2f055dc579f9dc5eace01fe2ed3665f0a2c7d6bb
ayman <aymanbelasri4@gmail.com>
2025-12-19 21:08:44 +0100

    tcp recv loop

--- vm3-compose/server.py
+++ vm3-compose/server.py
@@ -7,4 +7,8 @@ server.bind("0.0.0.0",9999)
 
 server.listen()
 
+while True:
+    client_socket , address = server.accept()
+    client_data= (client_socket.recv(4096)).decode("utf-8")
+    client_socket.close()
     
