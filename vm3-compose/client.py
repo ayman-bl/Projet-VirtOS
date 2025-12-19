9dc919668edae158124abe3fdedb4fc383de2fba
ayman <aymanbelasri4@gmail.com>
2025-12-19 22:31:28 +0100

    bind fix

--- vm3-compose/client.py
+++ vm3-compose/client.py
@@ -2,6 +2,6 @@ import socket
 
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-client.connect("192.168.200.28",9999) 
+client.connect(("192.168.200.28",9999)) 
 
 client.encode("utf-8").sendall()
